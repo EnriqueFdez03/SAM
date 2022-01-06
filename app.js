e0f6181292fcc531cb0286a11b78360a17247cbf
Enrique <enrique.fdezco@gmail.com>
2022-01-06 22:54:56 +0100

    Configuración variables de entorno

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { createServer } = require("http");
+require('dotenv').config();
 
+const port = process.env.PORT;
 const app = express();
 const server = createServer(app);
 
 
 app.use('/',express.static('public'));
-server.listen(8080, () => {console.log('servidor corriendo en el 8080')});
+server.listen(port, () => {console.log(`corriendo en el puerto ${port}`)});
